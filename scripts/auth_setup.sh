47fc038ced5d05c24b143280e73b945b1142aad4
Eliza Danyi <eliza.danyi@gmail.com>
2022-08-01 13:46:50 -0400

    modified visudo content

--- scripts/auth_setup.sh
+++ scripts/auth_setup.sh
@@ -7,17 +7,18 @@
 #########################################################
 
 ## Set variables for intended user
-export user_id=''
+export user_id='havok2'
 
 
 ## Add public key to autorized keys and ensure permissions are correct
-mkdir -p /home/$user_id/.ssh/ && chmod 700 /home/$user_id/.ssh && touch /home/$user_id/.ssh/authorized_keys && chmod 600 /home/$user_id/.ssh/authorized_keys && curl https://raw.githubusercontent.com/onproton/home-lab-work/main/scripts/resources/emd_ed25519.pub >> /home/$user_id/.ssh/authorized_keys 
+mkdir -p /home/$user_id/.ssh/ && chmod 700 /home/$user_id/.ssh && touch /home/$user_id/.ssh/authorized_keys && chmod 600 /home/$user_id/.ssh/authorized_keys && curl https://raw.githubusercontent.com/onproton/home-lab-work/main/scripts/resources/emd_ed25519.pub >> /home/$user_id/.ssh/authorized_keys && chown -R $user_id:$user_id /home/$user_id/ 
 
 ## Set users using keys and not passwords up to sudo without a password in sudoers
-echo ''$user_id' ALL=(ALL:ALL) ALL' | sudo EDITOR='tee -a' visudo
+echo ''$user_id' ALL=(ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo
 
 ## Change the default sshd_config for openssh access
 ## Create a backup of the current config file for reference
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bck
 ### Turn off password authentication, root login
-sudo curl TBD > /etc/ssh/sshd_config
+sudo curl https://raw.githubusercontent.com/onproton/home-lab-work/main/scripts/resources/sshd_config_default > /etc/ssh/sshd_config
+
